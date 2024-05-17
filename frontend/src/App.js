7016e6586f1d510b2567d9d6845984b93991c51e
JEEVAABI15 <jeevaabishake2002@gmail.com>
2024-05-17 22:42:35 +0530

    Frontend : Loginand signup added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LoginComponent from './components/loginComponent/loginComponent';
+import SignupComponent from './components/signupComponent/signupComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className='navbar navbar-expand-lg navbar-light fixed-top'>
+          <div className='container'>
+            <Link className='navbar-brand' to={'/'}>
+              Book My Movie
+            </Link>
+            <div id='navbarTogglerDemo02'>
+              <ul className='navbar-nav ml-auto'>
+                <li className='nav-item'>
+                  <Link className='nav-link' to={'/login'}>Login</Link>
+                </li>
+                <li className='nav-item'>
+                  <Link className='nav-link' to={'/signup'}>Sign Up</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className='auth-wrapper'>
+          <div className='auth-inner'>
+            <Routes>
+              <Route exact path='/' element={<LoginComponent />} />
+              <Route path='/login' element={<LoginComponent />} />
+              <Route path='/signup' element={<SignupComponent />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
